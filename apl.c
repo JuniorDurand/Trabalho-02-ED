29a7cec711927bfc5b5770274057142fd2a50dae
DURAND <junior_durand@outlook.com>
2019-04-19 17:26:11 -0300

    criando meios para inserir na coleção.

--- apl.c
+++ apl.c
@@ -1,13 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "colecao.h"
+#include "myLib.c"
+
+
+int main(){
+	int TotalElms, opcao = 0;
+	printf("Digite capacidade total da colecao:");
+	scanf("%d", &TotalElms);
+	Col *pessoas = colCreat(TotalElms);
+	if(pessoas == NULL){
+		return 0;// TESTA SE FOI ALOCADA A MATRIZ
+	}
+
+	do{
+		printMenu();
+		scanf("%d", &opcao);
+
+		if(opcao == 1){
+			Pessoa *p = (Pessoa*)malloc(sizeof(Pessoa));
+			printf("Digite o nome:");
+			fflush(stdin);
+			fgets(p->nome,50,stdin);
+			fflush(stdin);
+			printf("Digite a idade:");
+			scanf("%d", &(p->idade));
+			fflush(stdin);
+			printf("Digite a quantidade de filhos:");
+			scanf("%d", &(p->numFilhos));
+			fflush(stdin);
+			printf("Digite o salario:");
+			scanf("%d", &(p->salario));
+			fflush(stdin);
+			printf("Digite o CPF:");
+			fgets(p->CPF,15,stdin);
+			int flag = colInsert(pessoas, (void*)p);
+			if(flag){
+				printf("Elemento inserido com sucesso\n");
+			}else{
+				printf("Elemento não inserido na colecao\n");
+			}
+
+		}
 
 
 
-typedef struct _pessoa_{
-	char nome[50];
-	int idade;
-	int numFilhos;
-	float salario;
-	char CPF[15];
-}Pessoa;
+	}while(opcao != 5);
+	return 0;
+}
+
--- myLib.c
+++ myLib.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "colecao.h"
 
+
+typedef struct _pessoa_{
+	char nome[50];
+	int idade;
+	int numFilhos;
+	float salario;
+	char CPF[15];
+}Pessoa;
 
 
 void printMenu(){
@@ -12,3 +21,4 @@ void printMenu(){
 	printf("5 - Destruir estrutura e sair\n");
 	printf("Digite uma opcao: \n");
 }
+
