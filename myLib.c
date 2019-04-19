dec727ed291cf9c661ad8afba3da0005e61dbc3c
DURAND <junior_durand@outlook.com>
2019-04-19 17:02:44 -0300

    criando biblioteca interna para uso da aplicação.

---
+++ myLib.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+void printMenu(){
+	printf("1 - Inserir elemento\n");
+	printf("2 - Consultar elemento\n");
+	printf("3 - Remover elemento\n");
+	printf("4 - Listar todos os elementos\n");
+	printf("5 - Destruir estrutura e sair\n");
+	printf("Digite uma opcao: \n");
+}
