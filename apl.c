4db81aa0264e05bd93309af997cb097be5f9111a
DURAND <junior_durand@outlook.com>
2019-04-19 16:52:55 -0300

    criando arquivo de aplicação e estrutura Pessoa.

---
+++ apl.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "colecao.h"
+
+
+
+typedef struct _pessoa_{
+	char nome[50];
+	int idade;
+	int numFilhos;
+	float salario;
+	char CPF[15];
+}Pessoa;
